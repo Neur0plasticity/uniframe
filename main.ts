29f373b3448c26f5b3e27d5ffd45ab1830370833
djtinkers <djtinkers365@gmail.com>
2020-02-04 14:04:06 -0500

    specced

---
+++ main.ts
@@ -0,0 +1,68 @@
+export interface frame_obj {
+    namespace,
+    params,
+    bootstrap,
+    application,
+    design_patterns,
+    funC,
+    claS,
+}
+export const uniframe = function(frame_obj:frame_obj) {
+
+    const frame:frame_obj = frame_obj;
+
+};
+const folders = Object([
+    "DOCS",
+    "CODE",
+    "DEPLOY",
+    "MODULES",
+    "CLI",
+    "MISC",
+    "SPEC",
+    "DEV",
+    "PROD",
+    "_3RDPARTY",
+    "INHOUSE"
+]);
+const files = [
+    "README.md",
+    "misc.txt",
+    "requirements.txt",
+    "tasks.txt",
+    "bugs.txt",
+    "server.txt",
+    "database.txt",
+    "userinterface.txt"
+];
+const basefolders = [
+
+];
+const basefolder_DOCS = [
+    // "README.md",
+    // "misc.txt",
+    // "requirements.txt",
+    // "tasks.txt",
+    // "bugs.txt",
+    // "server.txt",
+    // "database.txt",
+    // "userinterface.txt"
+];
+const basefolder_CODE = [
+    // "spec",
+    // "development",
+    // "production"
+];
+const basefolder_DEPLOY     = [];
+const folder_MODULES    = [
+    // "_3rdparty",
+    // "inhouse"
+];
+const basefolder_CLI        = [];
+const basefolder_MISC       = [];
+
+const filesystem = function() {
+
+
+
+};
---
+++ spec.ts
@@ -0,0 +1,44 @@
+import { uniframe } from './main';
+
+const alg = uniframe;
+console.log(alg);
+
+console.warn(`
+    * All modules
+    * transpiler support
+    * credentials storage
+    * Sandbox-js
+    * Dependency Injection Management
+    * Namespace
+    * Params
+    * Bootstrap
+    * Design Patterns on standby
+    * effortless support funC
+    * effortless support claS
+    * (e2e & segmented) versioning spec testing
+    * proper naming sytems like
+    --- uil     replaces backend    user interface logic
+    --- uiv     replaces frontend   user interface visual
+    --- uia     replaces audio      user interface audio
+`);
+console.warn(`
+    filesystem design:
+        <miscellaneous files/folders>
+        <custom CLI commands>
+        <DEPLOY>
+        <all modules in one flat folder ... no nestations allowed>
+        <spec folder>
+        <documentation>
+        <entire app is data input only>
+`);
+(function TODOLIST(list:string[]){
+    console.log("TODOLIST");
+    console.log(...list);
+})([
+    "Fix all modules required to import into UNIFRAME",
+    "Program project folder structure",
+    "Program E2E/Segmented Tester",
+    "Program Codel File structure",
+    "Program CLI",
+    "Program UNIFRAME"
+]);
