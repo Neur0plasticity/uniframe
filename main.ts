90814cbd727b8c8939532e534835b65f3b153864
djtinkers <djtinkers365@gmail.com>
2020-04-14 12:44:49 -0400

    last mac upload

--- main.ts
+++ main.ts
@@ -1,68 +1,30 @@
-export interface frame_obj {
-    namespace,
-    params,
-    bootstrap,
-    application,
-    design_patterns,
-    funC,
-    claS,
-}
+export interface frame_obj {namespace,params,bootstrap,application,design_patterns,funC,claS};
 export const uniframe = function(frame_obj:frame_obj) {
-
     const frame:frame_obj = frame_obj;
-
 };
-const folders = Object([
-    "DOCS",
-    "CODE",
-    "DEPLOY",
-    "MODULES",
-    "CLI",
-    "MISC",
-    "SPEC",
-    "DEV",
-    "PROD",
-    "_3RDPARTY",
-    "INHOUSE"
-]);
-const files = [
-    "README.md",
-    "misc.txt",
-    "requirements.txt",
-    "tasks.txt",
-    "bugs.txt",
-    "server.txt",
-    "database.txt",
-    "userinterface.txt"
-];
-const basefolders = [
+const files     = [
+"00 README.md           'write your onboarding walk through'        'console.log(`readme created`);'",
+"01 misc.txt            'write your miscellaneous information'      'console.log(`miscellaneous requires extra user effort`);'",
+"02 tasks.txt           'always list contractable work'             'console.log(`tasks requires admins effort`);'",
+"03 requirements.txt    'requirements come first'                   'console.log(`tasks requires admins effort`);'",
+"04 bugs.txt            'always fix bugs'                           'console.log(`bugs  requires admins effort`);'",
+"05 server.txt          'rapid CLIs awesome'                        'console.log(`server is responsive`);'",
+"06 database.txt        'information everywhere'                    'console.log(`database is responsive,compact,zen`);'",
+"07 interface.txt       'always connecting with something'          'console.log(`interface everywhere or die of no use`);'",
+"08 modules.txt         'use everywhere'                            'console.log(`modules everywhere bulletproof testing`);' ",
+];files.forEach(e=>{{
+    
+}});
+const f = files;
+const folders   = Object([
+`00 CODE       :${f.slice()}`,   `03 DOCS       :${f.slice(0,1)}`,  
+`01 DEV        :${f.slice()}`,   `04 DEPLOY     :${f.slice(0,1)}`,
+`02 PROD       :${f.slice()}`,   `05 MODULES    :${f.slice(0,1)}`,     
+                                 `06 CLI        :${f.slice(0,1)}`,                                      
+`07 MISC       :${f.slice(0,3)}`,`09 _3RDPARTY  :${f.slice(0,5)}`, 
+`08 SPEC       :${f.slice(0,4)}`,`10 INHOUSE    :${f.slice(0,5)}`,
+                                `                                
+]); 
+const baseFolders = folders.slice(0,6);
 
-];
-const basefolder_DOCS = [
-    // "README.md",
-    // "misc.txt",
-    // "requirements.txt",
-    // "tasks.txt",
-    // "bugs.txt",
-    // "server.txt",
-    // "database.txt",
-    // "userinterface.txt"
-];
-const basefolder_CODE = [
-    // "spec",
-    // "development",
-    // "production"
-];
-const basefolder_DEPLOY     = [];
-const folder_MODULES    = [
-    // "_3rdparty",
-    // "inhouse"
-];
-const basefolder_CLI        = [];
-const basefolder_MISC       = [];
-
-const filesystem = function() {
-
-
-
-};
+const filesystem = function() {};
